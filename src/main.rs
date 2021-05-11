af207ca815ffb14948a5ff69539f47aec501af98
Ciremun <Ciremun@yandex.ru>
2021-05-11 15:08:44 +0300

    hello window

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use gl::types::*;
+use glfw::{Action, Context, Key};
+
+fn main() {
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+    let (mut window, events) = glfw.create_window(300, 300, "Hello window", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+
+    window.set_key_polling(true);
+    window.make_current();
+
+    gl::load_with(|s| window.get_proc_address(s) as *const _);
+    gl::Viewport::load_with(|s| window.get_proc_address(s) as *const _);
+
+    while !window.should_close() {
+        window.swap_buffers();
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            handle_window_event(&mut window, event);
+        }
+        unsafe { gl::ClearColor(0.2, 0.3, 0.3, 1.0); }
+        unsafe { gl::Clear(gl::COLOR_BUFFER_BIT); }
+    }
+}
+
+fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
+    match event {
+        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+            window.set_should_close(true)
+        }
+        _ => {}
+    }
+}
