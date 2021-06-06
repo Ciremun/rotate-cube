c5fbea3c9df59f2177904bab22502eb59b5ecc52
Ciremun <Ciremun@yandex.ru>
2021-06-06 17:00:10 +0300

    triangle lulw

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use std::mem;
+use std::ffi::*;
+
 use gl::types::*;
 use glfw::{Action, Context, Key};
 
 fn main() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
 
-    let (mut window, events) = glfw.create_window(300, 300, "Hello window", glfw::WindowMode::Windowed)
+    let (mut window, events) = glfw.create_window(800, 600, "Hello window", glfw::WindowMode::Windowed)
         .expect("Failed to create GLFW window.");
 
     window.set_key_polling(true);
@@ -13,14 +16,41 @@ fn main() {
     gl::load_with(|s| window.get_proc_address(s) as *const _);
     gl::Viewport::load_with(|s| window.get_proc_address(s) as *const _);
 
+    unsafe {
+        let mut vertex_array_id = 0;
+        gl::GenVertexArrays(1, &mut vertex_array_id);
+        gl::BindVertexArray(vertex_array_id);
+    }
+
+    let vertex_buffer_data = vec![
+        -1.0, -1.0, 0.0,
+        1.0, -1.0, 0.0,
+        0.0,  1.0, 0.0,
+    ];
+
+    let mut vertex_buffer = 0;
+    unsafe {
+        gl::GenBuffers(1, &mut vertex_buffer);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vertex_buffer);
+        gl::BufferData(gl::ARRAY_BUFFER, (mem::size_of::<f32>() * vertex_buffer_data.len()) as isize, vertex_buffer_data.as_ptr() as *const c_void, gl::STATIC_DRAW);
+    }
+
     while !window.should_close() {
         window.swap_buffers();
         glfw.poll_events();
         for (_, event) in glfw::flush_messages(&events) {
             handle_window_event(&mut window, event);
         }
-        unsafe { gl::ClearColor(0.2, 0.3, 0.3, 1.0); }
-        unsafe { gl::Clear(gl::COLOR_BUFFER_BIT); }
+        unsafe {
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            gl::EnableVertexAttribArray(0);
+            gl::BindBuffer(gl::ARRAY_BUFFER, vertex_buffer);
+            gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 0, 0 as *const c_void);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            gl::DisableVertexAttribArray(0);
+        }
     }
 }
 
